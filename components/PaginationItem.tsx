abbf1baf15bc60280fd41b38d29eb90f3cdd790e
ramiel99 <ramielwan48@gamil.com>
2022-02-22 20:07:13 +0200

    components/PaginationItem.tsx: checkpoint

--- components/PaginationItem.tsx
+++ components/PaginationItem.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import Animated, { Extrapolate, interpolate, useAnimatedStyle } from "react-native-reanimated";
+import { SCREEN_WIDTH } from "../consants";
 
 const ITEM_SIZE = 10;
 
-const PaginationItem = () => {
-    return <View style={styles.container}></View>;
+interface PaginationItem {
+    length: number;
+    index: number;
+    animationValue: Animated.SharedValue<number>;
+}
+
+const PaginationItem: React.FC<PaginationItem> = ({ animationValue, index, length }) => {
+    const rStyles = useAnimatedStyle(() => {
+        if (animationValue.value <= 0) {
+            const inputRange = [(index - 1) * SCREEN_WIDTH, index * SCREEN_WIDTH, (index + 1) * SCREEN_WIDTH];
+            const scale = interpolate(-animationValue.value, inputRange, [0.5, 1, 0.5], Extrapolate.CLAMP);
+            return {
+                transform: [{ scale: scale }],
+            };
+        } else {
+            const inputRange = [(length - index - 1) * SCREEN_WIDTH, (length - index) * SCREEN_WIDTH, (length - index + 1) * SCREEN_WIDTH];
+            const scale = interpolate(animationValue.value, inputRange, [0.5, 1, 0.5], Extrapolate.CLAMP);
+            return {
+                transform: [{ scale: scale }],
+            };
+        }
+    }, []);
+
+    return <Animated.View style={[styles.container, rStyles]} />;
 };
 
 export default PaginationItem;
